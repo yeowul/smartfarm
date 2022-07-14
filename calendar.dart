f6f347ff29c137b646c30113cb14f2d67ec6367a
yeowul <97037443+yeowul@users.noreply.github.com>
2022-07-14 18:53:56 +0900

    Add files via upload

---
+++ calendar.dart
@@ -0,0 +1,29 @@
+﻿import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+void main() => runApp(MaterialApp(
+      title: "App",
+      home: CalendarRoute(),
+    ));
+
+class CalendarRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('calendar Route'),
+      ),
+      body: Center(
+        child: OutlinedButton(
+          child: Text('Move Main route'),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => MainRoute()));
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ main.dart
@@ -0,0 +1,93 @@
+﻿import 'package:flutter/material.dart';
+import 'package:project/calendar.dart';
+
+void main() {
+  runApp(const MainRoute());
+}
+
+class MainRoute extends StatelessWidget {
+  const MainRoute({Key? key}) : super(key: key);
+
+// This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: '스마트팜'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+
+      backgroundColor: Colors.red[50],
+
+      body: Center(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: OutlinedButton(
+              onPressed: () {
+// Respond to button press
+              },
+              child: Text("재배 캘린더"),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: SizedBox(
+              child: OutlinedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => CalendarRoute())
+                  );
+
+// Respond to button press
+                },
+                child: Text("기후"),
+              ),
+            ),
+          ),
+        ],
+      )),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
